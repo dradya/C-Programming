70941920c8204e7de847348f8104e6ed83788538
dradya <adyatirvir29@gmail.com>
2024-08-04 14:07:52 +0530

    created project2

--- project2.c
+++ project2.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+main()
+{
+int f,g,h,i;
+printf("enter four numbers=\n");
+scanf("%d%d%d%d",&f,&g,&h,&i);
+if ((f<g)&&(f<h)&&(f<i))
+printf("\n %d is smallest",f);
+else if ((g<f)&&(g<h)&&(g<i))
+printf("\n %d is smallest",g);
+else if ((h<f)&&(h<g)&&(h<i))
+printf("\n %d is smallest",h);
+else
+printf("\n %d is smallest",i);
+}
