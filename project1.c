20c50f14e5483a19c032cf13eaf16468b60f987f
dradya <adyatirvir29@gmail.com>
2024-08-04 13:45:45 +0530

    new file

--- project1.c
+++ project1.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+main()
+{
+int c,d,e;
+printf("\n enter three numbers=");
+scanf("%d %d %d", &c, &d, &e);
+if(c>d)
+{
+if(c>e)
+printf("\n %d is greatest",c);
+else
+printf("\n %d is greatest",e);
+}
+else
+{
+if(d>e)
+printf("%d is greatest",d);
+else
+printf("\n %d is greatest",e);
+}
+}
