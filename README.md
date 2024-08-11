bf03201f5e5f4d2c46d9fffaacc33628f11ef1a7
dradya <adyatirvir29@gmail.com>
2024-08-11 13:59:34 +0530

    p3

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 operators.c - program to use various operatorn in c
 project1.c - program to display greatest of three numbers
 project2.c - program to display smallest of four numbers
-project3.c - program to fint if a number is positive or negative
+project3.c - program to find if a number is positive or negative
--- project3.c
+++ project3.c
@@ -1,1 +1,19 @@
+//program to find if a number is positive or negative
 #include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter a number=");
+    scanf("%d",&a);
+    if(a>=0)
+        printf("\n%d is positive",a);
+    else
+        printf("\n%d is negative",a);
+    
+    if(a%2==0)
+        printf("\n%d is even",a);
+    else
+        printf("\n%d is odd",a);
+    if(a==0)
+        printf("\n%d is zero");
+}
