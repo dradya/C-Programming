6bf7d5f667f30379c00004901a9b5a0e18b8bd75
dradya <adyatirvir29@gmail.com>
2024-09-05 14:31:43 +0530

    fibo

--- project15.c
+++ project15.c
@@ -2,5 +2,15 @@
 #include<stdio.h>
 void main()
 {
-    int 
+    int n,fib1=0,fib2=1,fib3,i;
+    printf("enter n=");
+    scanf("%d",&n);
+    printf("\n%d\t%d",fib1,fib2);
+    for(i=3;i<=n;i++);
+    {   
+        fib3=fib1+fib2;
+        printf("\t%d",fib3);
+        fib1=fib2;
+        fib2=fib3;
+    }
 }
